0e18de2e46f94aa957bd646d648ee67e5b29bd93
Viggo Valfridsson <viggovalf@gmail.com>
2025-11-16 21:06:36 +0100

    Add exercise 1-4

---
+++ src/1-4.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    float celsius = 0;
+    int upper = 300;
+    int step = 20;
+
+    printf("%3s %6s\n", "c", "f");
+
+    while (celsius <= upper) {
+        float fahr = (celsius * (9.0 / 5.0)) + 32.0;
+        printf("%3.0f %6.0f\n", celsius, fahr);
+
+        celsius += step;
+    }
+
+    return 0;
+}
