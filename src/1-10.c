df3729277c0de0d582fe932665f73310c8153e03
Viggo Valfridsson <viggovalf@gmail.com>
2025-11-18 21:28:41 +0100

    Add exercise 1-10

---
+++ src/1-10.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int character;
+
+    while ((character = getchar()) != EOF) {
+        if (character == '\t') {
+            printf("\\t");
+        } else if (character == '\b') {
+            printf("\\b");
+        } else if (character == '\\') {
+            printf("\\\\");
+        } else {
+            putchar(character);
+        }
+    }
+
+    return 0;
+}
