0f4992fc4f868e72af71007eb41129714156fbc0
Viggo Valfridsson <viggovalf@gmail.com>
2025-11-21 21:02:50 +0100

    Add exercise 1-13 pt. 2 and minor tweak to 1-13 pt. 1

--- src/chapter-1/1-13-horizontal.c
+++ src/chapter-1/1-13-horizontal.c
@@ -13,7 +13,7 @@ int main() {
     bool is_last_space = true;
     int character;
 
-    while ((character = getchar()) != EOF && word_index < 1024) {
+    while ((character = getchar()) != EOF && word_index < MAX_WORD_COUNT) {
         bool is_current_space = character == ' ' || character == '\t' || character == '\n';
         if (is_current_space && !is_last_space) {
             is_last_space = true;
---
+++ src/chapter-1/1-13-vertical.c
@@ -0,0 +1,80 @@
+// Write a program to print a histogram of the lengths of words in its input. It is easy to draw the histogram with the
+// bars horizontal; a verical orientation is more challenging
+// This file contains the horizontal solution, the vertical one can be found in 1-13-vertical.c
+// I decided to start splitting up the main function, as this example was quite a lot larger than previous exercises
+
+#include <stdbool.h>
+#include <stdio.h>
+
+#define MAX_WORD_COUNT 1024
+
+int read_word_lengths(int *word_lengths, int max_words) {
+    int word_index = 0;
+    bool is_last_space = true;
+    int character;
+
+    while ((character = getchar()) != EOF && word_index < max_words) {
+        bool is_current_space = character == ' ' || character == '\t' || character == '\n';
+
+        if (is_current_space && !is_last_space) {
+            is_last_space = true;
+            word_index++;
+        } else if (!is_current_space) {
+            is_last_space = false;
+            word_lengths[word_index]++;
+        } else {
+            is_last_space = is_current_space;
+        }
+    }
+
+    // After loop, account for last word if it didn't end with space
+    if (!is_last_space) {
+        word_index++;
+    }
+
+    return word_index;
+}
+
+int find_max_word_len(int *word_lengths, int word_index) {
+    int longest_word_length = 0;
+
+    for (int i = 0; i < word_index; i++) {
+        int current_length = word_lengths[i];
+
+        if (current_length > longest_word_length) {
+            longest_word_length = current_length;
+        }
+    }
+
+    return longest_word_length;
+}
+
+void print_histogram_bars(int longest_word_length, int *word_lengths, int word_count) {
+    for (int row = longest_word_length; row > 0; row--) {
+        for (int column = 0; column < word_count; column++) {
+            printf(word_lengths[column] >= row ? "|||| " : "     ");
+        }
+        putchar('\n');
+    }
+}
+
+void print_histogram_header(int word_count) {
+    for (int i = 0; i < word_count; i++) {
+        printf("%4d ", i + 1);
+    }
+
+    putchar('\n');
+}
+
+int main() {
+    int word_lengths[MAX_WORD_COUNT] = {0};
+
+    int word_count = read_word_lengths(word_lengths, MAX_WORD_COUNT);
+    int longest_word_length = find_max_word_len(word_lengths, word_count);
+
+    print_histogram_bars(longest_word_length, word_lengths, word_count);
+    print_histogram_header(word_count);
+
+    return 0;
+}
+
